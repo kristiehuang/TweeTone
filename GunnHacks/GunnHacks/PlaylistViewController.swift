970f9c3d50c528fd3bf43eafe9fa26518455178e
Kristie Huang <kristiehuang01@gmail.com>
2017-10-27 23:33:25 -0700

    Storyboard commit

--- GunnHacks/GunnHacks/PlaylistViewController.swift
+++ GunnHacks/GunnHacks/PlaylistViewController.swift
@@ -10,8 +10,21 @@ import Foundation
 import UIKit
 
 class PlaylistViewController: UIViewController {
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var moodImage: UIImageView!
+    
+    @IBOutlet weak var playlistButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    
+    @IBAction func playlistButtonTapped(_ sender: Any) {
+        
+        
+    }
+    
+    
+    
 }
