f10ab52d1160e4c69b7bcde5dd6466120e116f16
19jacobk <jacobjk01@gmail.com>
2017-10-27 22:13:15 -0700

    reset

--- GunnHacks/GunnHacks/ViewController.swift
+++ GunnHacks/GunnHacks/ViewController.swift
@@ -7,56 +7,14 @@
 //
 
 import UIKit
-import CoreMotion
 
 class ViewController: UIViewController {
     
-    var motionManager = CMMotionManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(true)
-        print("wwww")
-        
-        motionManager.gyroUpdateInterval = 0.2
-        
-        let dispatchQueue = DispatchGroup()
-        dispatchQueue.enter()
-        
-        dispatchQueue.notify(queue: .main, execute: {
-            print("hello pls work")
-        })
-        
-        
-        motionManager.startGyroUpdates(to: OperationQueue.current!) { (data, error) in
-
-            print("asdfjskaf")
-
-            if let myData = data {
-                print (myData.rotationRate)
-                
-                if myData.rotationRate.x > 3 {
-                    print("X direction turned")
-                }
-                
-                if myData.rotationRate.y > 3 {
-                    print("Y direction turned")
-                }
-                
-                if myData.rotationRate.z > 3 {
-                    print("Z direction turned")
-                }
-            }
-            
-            dispatchQueue.leave()
-        }
-    }
-    
-    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
